11f736e317f5f98c29cddd9b8c8075e33bbd4896
Dylan Bomgardner <dylan.m.bomgardner@gmail.com>
2024-01-07 23:15:54 -0800

    test

--- inc/gpio.h
+++ inc/gpio.h
@@ -4,6 +4,17 @@
 #define CLK_APB2 CLOCK_BASE_ADDRESS + 0x18
 #define GPIOA_BASE 0x40010800U
 
+typedef struct GPIO {
+    uint32_t CRL;
+    uint32_t CRH;
+    uint32_t IDR;
+    uint32_t ODR;
+    uint32_t BSRR;
+    uint32_t LCKR;
+    uint32_t AFIO_EVCR;
+}GPIO;
+
+
 /*
     Function Definitions.
 */
@@ -15,3 +26,6 @@
 void gpio_setup(void);
 void gpio_enable(void);
 void gpio_pin_setup(void);
+
+void gpio_set(uint8_t GPIO_PORT, uint8_t GPIO_PIN);
+void gpio_clear(uint8_t GPIO_PORT, uint8_t GPIO_PIN)
